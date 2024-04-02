cc0d4aad897e48c54bba10a1abbb86b68172ac33
JanLam <honos628@foxmail.com>
2024-04-02 17:25:29 +0800

    dev: When replacing the secretFile, back up the old secret file first to avoid losing it.

--- cmd/secret.go
+++ cmd/secret.go
@@ -8,7 +8,7 @@ import (
 )
 
 var (
-	secretForce bool
+	secretReplace bool
 	// secretCmd represents the secret command
 	secretCmd = &cobra.Command{
 		Use:     "secret",
@@ -16,17 +16,31 @@ var (
 		Aliases: []string{"sec"},
 		Run: func(cmd *cobra.Command, _ []string) {
 			s := secret.GenerateSecret()
-			err := s.Save(secretForce)
+			var err error
+			var newerSec *secret.Secret
+			if secretReplace {
+				newerSec, err = s.Replace()
+			} else {
+				err = s.Save()
+			}
+
 			if err != nil {
 				logger.Error("error generating secret", "err", err.Error())
 				os.Exit(1)
 			}
-			logger.Info("secret generated", "filepath", s.Path())
+
+			if newerSec != nil && newerSec.Path() != s.Path() {
+				logger.Info("secret file has been replaced",
+					"newer", newerSec.Path(),
+					"backup", s.Path())
+			} else {
+				logger.Info("secret generated", "filepath", s.Path())
+			}
 		},
 	}
 )
 
 func init() {
 	rootCmd.AddCommand(secretCmd)
-	secretCmd.Flags().BoolVarP(&secretForce, "force", "f", false, "force overwrite secret file")
+	secretCmd.Flags().BoolVarP(&secretReplace, "replace", "r", false, "replace secret file")
 }
--- internal/secret/secret.go
+++ internal/secret/secret.go
@@ -2,8 +2,10 @@ package secret
 
 import (
 	"errors"
+	"fmt"
 	"os"
 	"path/filepath"
+	"time"
 
 	"github.com/linlanniao/soss/pkg/utils"
 )
@@ -26,7 +28,7 @@ var (
 	savePath = filepath.Join(home, ".soss", ".secret")
 )
 
-func (secret *Secret) Save(replace bool) error {
+func (secret *Secret) Save() error {
 	if secret == nil {
 		return errors.New("secret is nil")
 	}
@@ -43,11 +45,50 @@ func (secret *Secret) Save(replace bool) error {
 		}
 	}
 
-	if utils.IsFile(secret.path) && !replace {
+	if utils.IsFile(secret.path) {
 		return errors.New("secret file already exists")
 	}
 
 	return os.WriteFile(secret.path, []byte(secret.key), 0600)
+}
+
+func (secret *Secret) Replace() (newerSecret *Secret, err error) {
+	if secret == nil {
+		return nil, errors.New("secret is nil")
+	}
+
+	if len(secret.key) == 0 {
+		return nil, errors.New("key is empty")
+	}
+
+	dir := filepath.Dir(secret.path)
+	if _, err := os.Stat(dir); os.IsNotExist(err) {
+		err = os.MkdirAll(dir, 0700)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	newer := &Secret{
+		key:  secret.key,
+		path: secret.path,
+	}
+
+	if utils.IsFile(newer.path) {
+		now := time.Now()
+		t := now.Format("2006-01-02_15-04-05")
+		backupFileName := fmt.Sprintf("%s.%s.backup", secret.path, t)
+		if err := os.Rename(secret.path, backupFileName); err != nil {
+			return nil, err
+		}
+		secret.path = backupFileName
+	}
+
+	err = os.WriteFile(newer.path, []byte(secret.key), 0600)
+	if err != nil {
+		return nil, err
+	}
+	return newer, nil
 }
 
 func (secret *Secret) Path() string {
